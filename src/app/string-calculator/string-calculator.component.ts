cfeadadf19edc12034a5ab28e0e9cc1e8862daf5
vkovaleva <vkovaleva@voxelgroup.net>
2022-02-06 21:39:55 +0100

    chore: create string-calculator component

---
+++ src/app/string-calculator/string-calculator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-string-calculator',
+  templateUrl: './string-calculator.component.html',
+  styleUrls: ['./string-calculator.component.css']
+})
+export class StringCalculatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
